f8a25bc0eedc01daa10f75baf4db4f16a0912519
neon1024 <robertscumnpu@gmail.com>
2025-09-06 18:13:05 +0300

    npx expo reset-project

---
+++ finance-tracking/app/index.tsx
@@ -0,0 +1,23 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "black",
+            }}
+        >
+            <Text
+                style={{
+                    color: "cyan",
+                    fontSize: 24
+                }}
+            >
+                neon1024
+            </Text>
+        </View>
+    );
+}
