7a35ff1db951d7c285b8a5a132e26f2b0dd6c2c1
neon1024 <robertscumnpu@gmail.com>
2025-09-22 00:49:12 +0300

    Update _layout.tsx

--- finance-tracking/app/_layout.tsx
+++ finance-tracking/app/_layout.tsx
@@ -1,111 +1,88 @@
 import { Octicons } from "@expo/vector-icons";
 import { Drawer } from "expo-router/drawer";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
     return (
-        <Drawer screenOptions={{ drawerItemStyle: { margin: 8 } }}>
-            <Drawer.Screen
-                name="index"
-                options={{
-                    title: "Home",
-
-                    drawerIcon: ({ size }) => (
-                        <Octicons name="home" size={size} color="cyan" />
-                    ),
-
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Drawer
+                screenOptions={{
+                    swipeEnabled: true,
+                    drawerItemStyle: { margin: 8 },
                     drawerLabelStyle: {
                         color: "cyan",
                     },
                     drawerActiveBackgroundColor: "black",
-
-                    headerTitle: "Finance Tracking",
                     headerStyle: {
                         backgroundColor: "black",
                     },
                     headerTintColor: "cyan",
                     headerTitleAlign: "center",
                 }}
-            />
-            <Drawer.Screen
-                name="account"
-                options={{
-                    title: "Account",
+            >
+                <Drawer.Screen
+                    name="index"
+                    options={{
+                        title: "Home",
 
-                    drawerIcon: ({ size }) => (
-                        <Octicons name="person" size={size} color="cyan" />
-                    ),
+                        drawerIcon: ({ size }) => (
+                            <Octicons name="home" size={size} color="cyan" />
+                        ),
 
-                    drawerLabelStyle: {
-                        color: "cyan",
-                    },
-                    drawerActiveBackgroundColor: "black",
+                        headerTitle: "Finance Tracking",
+                    }}
+                />
+                <Drawer.Screen
+                    name="account"
+                    options={{
+                        title: "Account",
 
-                    headerTitle: "Account",
-                    headerStyle: {
-                        backgroundColor: "black",
-                    },
-                    headerTintColor: "cyan",
-                    headerTitleAlign: "center",
-                }}
-            />
-            <Drawer.Screen
-                name="income"
-                options={{
-                    title: "Income",
+                        drawerIcon: ({ size }) => (
+                            <Octicons name="person" size={size} color="cyan" />
+                        ),
 
-                    drawerIcon: ({ size }) => (
-                        <Octicons
-                            name="arrow-up-right"
-                            size={size}
-                            color="cyan"
-                        />
-                    ),
+                        headerTitle: "Account",
+                    }}
+                />
+                <Drawer.Screen
+                    name="income"
+                    options={{
+                        title: "Income",
 
-                    drawerLabelStyle: {
-                        color: "cyan",
-                    },
-                    drawerActiveBackgroundColor: "black",
+                        drawerIcon: ({ size }) => (
+                            <Octicons
+                                name="arrow-up-right"
+                                size={size}
+                                color="cyan"
+                            />
+                        ),
 
-                    headerTitle: "Income",
-                    headerStyle: {
-                        backgroundColor: "black",
-                    },
-                    headerTintColor: "cyan",
-                    headerTitleAlign: "center",
-                }}
-            />
-            <Drawer.Screen
-                name="expenses"
-                options={{
-                    title: "Expenses",
+                        headerTitle: "Income",
+                    }}
+                />
+                <Drawer.Screen
+                    name="expenses"
+                    options={{
+                        title: "Expenses",
 
-                    drawerIcon: ({ size }) => (
-                        <Octicons
-                            name="arrow-down-right"
-                            size={size}
-                            color="cyan"
-                        />
-                    ),
+                        drawerIcon: ({ size }) => (
+                            <Octicons
+                                name="arrow-down-right"
+                                size={size}
+                                color="cyan"
+                            />
+                        ),
 
-                    drawerLabelStyle: {
-                        color: "cyan",
-                    },
-                    drawerActiveBackgroundColor: "black",
-
-                    headerTitle: "Expenses",
-                    headerStyle: {
-                        backgroundColor: "black",
-                    },
-                    headerTintColor: "cyan",
-                    headerTitleAlign: "center",
-                }}
-            />
-            <Drawer.Screen
-                name="+not-found"
-                options={{
-                    drawerItemStyle: { display: "none" },
-                }}
-            />
-        </Drawer>
+                        headerTitle: "Expenses",
+                    }}
+                />
+                <Drawer.Screen
+                    name="+not-found"
+                    options={{
+                        drawerItemStyle: { display: "none" },
+                    }}
+                />
+            </Drawer>
+        </GestureHandlerRootView>
     );
 }
