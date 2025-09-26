46fb27422f5f24bc146259c769dcfdb2cda20b17
neon1024 <robertscumnpu@gmail.com>
2025-09-26 20:28:52 +0300

    Expenses -> first section MVP

--- finance-tracking/app/expenses.tsx
+++ finance-tracking/app/expenses.tsx
@@ -1,30 +1,64 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import {
+    FlatList,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
+
+import { Octicons } from "@expo/vector-icons";
+
+import { useState } from "react";
+
 import Expense from "./Expense";
 
 // TODO use FlashList for better performance
 
 export default function Expenses() {
+    const months = [
+        { label: "January", value: 1 },
+        { label: "February", value: 2 },
+        { label: "March", value: 3 },
+        { label: "April", value: 4 },
+        { label: "May", value: 5 },
+        { label: "June", value: 6 },
+        { label: "July", value: 7 },
+        { label: "August", value: 8 },
+        { label: "September", value: 9 },
+        { label: "October", value: 10 },
+        { label: "November", value: 11 },
+        { label: "December", value: 12 },
+    ];
+
+    const [month, setMonth] = useState(0);
+
     return (
         <View style={styles.container}>
-            <Text>Expenses</Text>
-            <FlatList
-                style={{
-                    flex: 1,
-                }}
-                contentContainerStyle={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}
-                data={[
-                    new Expense({ name: "Breakfast", cost: 10 }),
-                    new Expense({ name: "Lunch", cost: 20 }),
-                    new Expense({ name: "Dinner", cost: 15 }),
-                    new Expense({ name: "Gas", cost: 30 }),
-                    new Expense({ name: "Groceries", cost: 40 }),
-                ]}
-                renderItem={({ item }) => <Text>{item.toString()}</Text>}
-            />
+            <View style={styles.monthPickerAndAddExpenseButtonContainer}>
+                <Picker
+                    style={styles.monthPicker}
+                    selectedValue={month}
+                    onValueChange={(selectedMonth) => setMonth(selectedMonth)}
+                >
+                    <Picker.Item label="Month" value={0} />
+                    {months.map((month) => (
+                        <Picker.Item
+                            key={month.value}
+                            label={month.label}
+                            value={month.value}
+                        />
+                    ))}
+                </Picker>
+                <TouchableOpacity style={styles.addExpenseButtonContainer}>
+                    <Octicons name="plus" size={24} color="#fff" />
+                    <Text style={styles.addExpenseButtonText}>Add Expense</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View>Chart + Total</View>
+            <View>Categories</View>
+            <View>Recent Expenses</View>
         </View>
     );
 }
@@ -32,7 +66,46 @@ export default function Expenses() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center",
+    },
+
+    monthPicker: {
+        borderRadius: 16,
+
+        padding: 8,
+
+        backgroundColor: "black",
+
+        color: "cyan",
+    },
+
+    monthPickerAndAddExpenseButtonContainer: {
+        flexDirection: "row",
+
+        justifyContent: "space-between",
+        alignItems: "center",
+
+        width: "90%",
+
+        marginTop: 24,
+    },
+
+    addExpenseButtonContainer: {
+        width: 200,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+
+        borderRadius: 16,
+
+        backgroundColor: "cyan",
+
+        padding: 8,
+    },
+
+    addExpenseButtonText: {
+        fontSize: 24,
+        color: "#fff",
     },
 });
